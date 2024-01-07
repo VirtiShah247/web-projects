523f4dce6d289a34c5e1819968fbee966067ba3f
Virti Shah <vpshah247@gmail.com>
2024-01-07 23:37:32 +0530

    Modify index.js code of mobile menu for better performance and add active state code

--- HTML-CSS-JS/news-homepage/index.js
+++ HTML-CSS-JS/news-homepage/index.js
@@ -1,34 +1,41 @@
-const bodyContent = document.getElementsByTagName("body")[0].innerHTML;
-const displayNavbar = () => {
-    let mobileNavbarLinks = `
-    <div class="mobileNavbarLinks">
-        <div>
-            <img src="./assets/images/icon-menu-close.svg" alt="close-menu" id="hamburgerCloseMenuImage"/>
-        </div>
-        <div>
-            <a href="#home" class="mobileNavbarLinkTag">Home</a>
-            <a href="#new" class="mobileNavbarLinkTag">New</a>
-            <a href="#popular" class="mobileNavbarLinkTag">Popular</a>
-            <a href="#trending" class="mobileNavbarLinkTag">Trending</a>
-            <a href="#categories" class="mobileNavbarLinkTag">Categories</a>
-        <div>
-    </div>
-   `
-    // document.getElementsByClassName("parentContainer")[0].innerHTML = "";
-    document.getElementsByClassName("mainContainer")[0].style.backgroundColor = "rgb(163, 159, 159)";
-    document.getElementsByClassName("webImageContainer")[0].style.opacity = "0.5";
-    document.getElementsByClassName("buttonContainer")[0].style.opacity = "0.5";
-    document.getElementsByClassName("rightComponent")[0].style.opacity = "0.5";
-    document.getElementsByTagName("body")[0].innerHTML += mobileNavbarLinks;
-    document.getElementsByClassName("mainContainer")[0].addEventListener('click', hideNavbar);
-}
-const hideNavbar = () => {
-    document.getElementsByTagName("body")[0].innerHTML = bodyContent;
-    document.getElementById("hamburgerMenuImage").addEventListener('click',displayNavbar);
-}
-document.addEventListener('click', function (event) {
-    if(event.target.id === 'hamburgerCloseMenuImage' || event.target.className === 'mobileNavbarLinkTag'){
-        hideNavbar();
+const mobileNavbar = (event) => {
+    if (event.target.id == "hamburgerMenuImage" || event.target.id == "mobileNavbarLinks") {
+        const elements = document.querySelectorAll(".webImageContainer,.buttonContainer,.rightComponent");
+        Object.keys(elements).forEach((key) => elements[key].style.opacity = "0.5");
+        document.getElementsByClassName("mainContainer")[0].style.backgroundColor = "rgb(163, 159, 159)";
+        document.getElementById("mobileNavbarLinks").style.display = "block";
     }
-})
-document.getElementById("hamburgerMenuImage").addEventListener('click',displayNavbar);
+    else {
+        const elements = document.querySelectorAll(".webImageContainer,.buttonContainer,.rightComponent");
+        Object.keys(elements).forEach((key) => elements[key].style.opacity = "1");
+        document.getElementsByClassName("mainContainer")[0].style.backgroundColor = "hsl(36, 100%, 99%)";
+        document.getElementById("mobileNavbarLinks").style.display = 'none';
+    }
+
+}
+const handleActiveTag = (activeTag) => {
+    // const activeTag = event.target.id;
+
+    // ['Home', 'New', 'Popular', 'Trending', 'Categories'].forEach((tag) => {
+
+    // })
+    for (let tag of ['Home', 'New', 'Popular', 'Trending', 'Categories']) {
+        if (tag === activeTag) {
+            document.getElementById(activeTag).classList.add("active");
+        }
+        else {
+            document.getElementById(tag).classList.remove("active");
+        }
+    }
+}
+document.getElementsByClassName("mainContainer")[0].addEventListener('click', mobileNavbar);
+document.getElementById("hamburgerMenuImage").onclick = mobileNavbar;
+document.getElementById("hamburgerCloseMenuImage").onclick = mobileNavbar;
+
+const url = window.location.href.split("#")[1];
+if (url === '') {
+    handleActiveTag('Home');
+}
+else {
+    handleActiveTag(url);
+}

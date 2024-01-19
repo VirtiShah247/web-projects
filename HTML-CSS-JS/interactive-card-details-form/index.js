a9c0a2a9c1da4691dbd6c08174283c6554c2b148
Virti Shah <vpshah247@gmail.com>
2024-01-19 19:36:04 +0530

    improve code, change cardNumber regex and add space after every 4 digit and wrap error message

--- HTML-CSS-JS/interactive-card-details-form/index.js
+++ HTML-CSS-JS/interactive-card-details-form/index.js
@@ -1,119 +1,93 @@
-const displayCardDetails = () => {
-    const card = document.card;
-    const cardHolderName = card.cardHolderName.value;
-    const cardNumber = card.cardNumber.value;
-    const expMonth = card.expMonth.value;
-    const expYear = card.expYear.value;
-    const CVC = card.CVC.value;
+const card = document.card;
+const cardHolderName = card.cardHolderName;
+const cardNumber = card.cardNumber;
+const expMonth = card.expMonth;
+const expYear = card.expYear;
+const CVC = card.CVC;
 
+const handleCardNumberChange = () =>{
+    cardNumber.value = cardNumber.value.match(/[0-9]{4}/g).join(" ");
+}
+
+const displayCardDetails = () => {
     const imageCardHolderName = document.getElementById("imageCardHolderName");
     const imageCardNumber = document.getElementById("imageCardNumber");
     const imageExpDate = document.getElementById("imageExpDate");
     const imageCVC = document.getElementById("imageCVC");
-    cardHolderName !== '' && (imageCardHolderName.innerHTML = cardHolderName);
-    cardNumber !== '' && (imageCardNumber.innerHTML = cardNumber);
-    expMonth !== '' && expYear !== '' && (imageExpDate.innerHTML = expMonth + "/" + expYear);
-    CVC !== '' && (imageCVC.innerHTML = CVC);
+    // console.log(cardNumber.value.split(""));
+    imageCardHolderName.innerHTML = cardHolderName.value || "Jane Appleseed";
+    imageCardNumber.innerHTML = cardNumber.value || "0000 0000 0000 0000";
+    imageExpDate.innerHTML = (expMonth.value && expYear.value) ? (expMonth.value + "/" + expYear.value) : "00/00";
+    imageCVC.innerHTML = CVC.value || "000";
 }
-const showErrorMessage = (id, inputName,message = null) => {
-    document.getElementById(id).style.display = "block";
-    document.forms["card"][inputName].style.borderColor = "hsl(0, 100%, 66%)";
-    message !== null && (document.getElementById(id).innerHTML = message);
-}
-const hideErrorMessage = (id, inputName) => {
-    document.getElementById(id).style.display = "none";
-    document.forms["card"][inputName].style.borderColor = "hsl(270, 3%, 87%)";
-} 
-const handleCardValidation = () => {
-    const cardNumberRegex = /^[0-9]{16}$/;
-    const monthRegex = /^(0[1-9]|1[0-2])$/;
-    const yearRegex = /^([0-9]{4}|[0-9]{2})$/;
-    const cvcRegex = /^[0-9]{3,4}$/;
 
-    const card = document.card;
-    const cardHolderName = card.cardHolderName.value;
-    const cardNumber = card.cardNumber.value;
-    const expMonth = card.expMonth.value;
-    const expYear = card.expYear.value;
-    const CVC = card.CVC.value;
-    let validated = true;
-    if(cardHolderName == ''){
-        showErrorMessage("cardHolderNameErrorMessage", "cardHolderName");
-        validated = false;
-    }
-    else{
-        hideErrorMessage("cardHolderNameErrorMessage", "cardHolderName");
-    }
-    if(cardNumber == ''){
-        showErrorMessage("cardNumberErrorMessage", "cardNumber");
-        validated = false;
-    }
-    else if(!cardNumberRegex.test(cardNumber)){
-        message = "Wrong format, numbers only";
-        showErrorMessage("cardNumberErrorMessage", "cardNumber", message);
-        validated = false;
-    }
-    else{
-        hideErrorMessage("cardNumberErrorMessage", "cardNumber");
-    }
-    if(expMonth == ''){
-        showErrorMessage("expMonthErrorMessage", "expMonth");
-        validated = false;
-    }
-    else if(!monthRegex.test(expMonth)){
-        message = "Wrong month format";
-        showErrorMessage("expMonthErrorMessage", "expMonth", message);
-        validated = false;
-    }
-    else{
-        hideErrorMessage("expMonthErrorMessage", "expMonth");
-    }
-    if(expYear == ''){
-        showErrorMessage("expYearErrorMessage", "expYear");
-        validated = false;
-    }
-    else if(!yearRegex.test(expYear)){
-        showErrorMessage("expYearErrorMessage", "expYear", message);
-        validated = false;
-    }
-    else{
-        hideErrorMessage("expYearErrorMessage", "expYear");
-    }
-    if(CVC == ''){
-        showErrorMessage("CVCErrorMessage", "CVC");
-        validated = false;
-    }
-    else if(!cvcRegex.test(CVC)){
-        message = "3 or 4 digit number only";
-        showErrorMessage("CVCErrorMessage", "CVC", message);
-        validated = false;
-    }
-    else{
-        hideErrorMessage("CVCErrorMessage", "CVC");
-    }
-    if(!validated){
+const errorMessage = {
+    cardNumber: "Wrong format, numbers only",
+    expMonth: "Wrong month format",
+    expYear: "Wrong year format",
+    CVC: "3 or 4 digit number only"
+}
+const validationRegex = {
+    cardHolderName: /.+/,
+    cardNumber: /((\d{4}[\s-]?){3}\d{4})/,
+    expMonth: /^(0[1-9]|1[0-2])$/,
+    expYear: /^([0-9]{4}|[0-9]{2})$/,
+    CVC: /^[0-9]{3,4}$/
+}
+
+const showErrorMessage = (inputName, message = null) => {
+    document.getElementById(inputName + "ErrorMessage").style.display = "flex";
+    document.forms["card"][inputName].style.borderColor = "hsl(0, 100%, 66%)";
+    message !== null && (document.getElementById(inputName + "ErrorMessage").innerHTML = message);
+}
+const hideErrorMessage = (inputName) => {
+    document.getElementById(inputName + "ErrorMessage").style.display = "none";
+    document.forms["card"][inputName].style.borderColor = "hsl(270, 3%, 87%)";
+}
+const validateCard = (field, regex) => {
+    // console.log(field, regex, field.name + "ErrorMessage", field.value);
+    if(field.value == ""){
+        showErrorMessage(field.name);
         return false;
     }
+    else if(!regex.test(field.value)){
+        showErrorMessage(field.name, errorMessage[field.name]);
+        return false;
+    }
+    else{
+        hideErrorMessage(field.name);
+    }
     return true;
+} 
+const handleFormValidation = () => {
+    let isValid = true;
+    isValid &= validateCard(cardHolderName, validationRegex.cardHolderName);
+    isValid &= validateCard(cardNumber, validationRegex.cardNumber);
+    isValid &= validateCard(expMonth, validationRegex.expMonth);
+    isValid &= validateCard(expYear, validationRegex.expYear);
+    isValid &= validateCard(CVC, validationRegex.CVC);
+    return isValid;   
 }
 const submitCardDetails = () => {
     event.preventDefault();
     displayCardDetails();
-    const state =  handleCardValidation();
+    const state =  handleFormValidation();
     if(state){
-        document.card.style.display = "none";
+        card.style.display = "none";
         document.getElementById("completeContainer").style.display = "flex";
     }
     return state;
 }
 const handleContinue = () => {
-    console.log("continue");
-    document.card.style.display = "grid";
+    card.style.display = "grid";
     document.getElementById("completeContainer").style.display = "none";
-    document.card.cardHolderName.value = null;
-    document.card.cardNumber.value = null;
-    document.card.expMonth.value = null;
-    document.card.expYear.value = null;
-    document.card.CVC.value = null;    
+    cardHolderName.value = "";
+    cardNumber.value = "";
+    expMonth.value = "";
+    expYear.value = "";
+    CVC.value = "";
+    displayCardDetails();    
 }
-document.querySelector("#completeContainer .buttonComponent").addEventListener('click', handleContinue);
+document.querySelector("#completeContainer .buttonComponent").addEventListener("click", handleContinue);
+card.addEventListener("submit", submitCardDetails);
+cardNumber.addEventListener("change", handleCardNumberChange);

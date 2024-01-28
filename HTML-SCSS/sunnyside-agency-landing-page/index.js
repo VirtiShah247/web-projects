4ddd7552eae8dde1824e6da95596ed0227f172ad
Virti Shah <vpshah247@gmail.com>
2024-01-28 23:59:26 +0530

    add hamburger menu functionality for mobile-friendly view

---
+++ HTML-SCSS/sunnyside-agency-landing-page/index.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector(".menu");
+const removeMobileMenu = () => {
+    menu.classList.remove("menu--mobile");
+    if(window.screen.width > 576){
+        menu.style.display = "grid"; 
+    }
+    else{
+        menu.style.display = "none"; 
+    }
+}
+const handleMenuClick = () => {
+    const menuMobile = menu.classList.value.split(" ")[1];
+    console.log(menuMobile);
+    console.log(window.screen.width);
+    if(menuMobile === undefined){
+        menu.classList.add("menu--mobile");
+        menu.style.display = "grid";
+    }
+    else{
+        menu.classList.remove("menu--mobile");
+        menu.style.display = "none"; 
+    }
+}
+document.querySelector(".hamburger__image").addEventListener("click", handleMenuClick);
+window.addEventListener("resize", removeMobileMenu);

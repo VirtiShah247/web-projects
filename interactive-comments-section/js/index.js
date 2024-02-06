e73f33738286d8daf50c56f5d9de633b621d881f
Virti Shah <vpshah247@gmail.com>
2024-02-06 16:18:30 +0530

    add functionality to reply

--- interactive-comments-section/js/index.js
+++ interactive-comments-section/js/index.js
@@ -1,6 +1,7 @@
 let user_data = JSON.parse(localStorage.getItem("data"));
 
 const findNextId = () => {
+    console.log("find next id array ",user_data.comments);
     let length = user_data.comments.length;
     user_data.comments.forEach(user => {
         length += user.replies.length;
@@ -9,28 +10,26 @@ const findNextId = () => {
     return length;
 }
 
-const addNewComment = () => {
+const addNewComment = async () => {
     event.preventDefault();
     let content = document.getElementById("comment__text");
     const contentValue = content.value.trim();
     console.log(contentValue);
     content.value = "";
-    const nextId = findNextId() + 1;
+    const nextId = await findNextId() + 1;
     console.log(nextId);
     const newComment = {
         "id": nextId,
         "content": contentValue,
         "score": 0,
-        "replies": [],
         "user": user_data.currentUser,
         "createdAt": "1 minute ago"
     }
-    // user_data.comments = [...user_data.comments, newComment];
-    user_data.comments.push(newComment);
+    const newUserData = JSON.parse(JSON.stringify(user_data));
+    newUserData.comments.push(newComment);
     console.log(user_data);
-    localStorage.setItem("data", JSON.stringify(user_data));
+    localStorage.setItem("data", JSON.stringify(newUserData));
     displayUser();
-
 }
 const increaseScore = (userId, replyStatus) => {
     user_data.comments.map((user) => {
@@ -89,11 +88,14 @@ const deleteComment = (userId, replyStatus) => {
 }
 const updateComment = (userId) => {
     const contentEdit = document.getElementById(`user__content--${userId}`);
-    contentEdit.contentEditable = false;
+    console.log(contentEdit.innerHTML);
     const buttonUpdate = document.getElementById(`button__update--${userId}`);
+    console.log(buttonUpdate);
     buttonUpdate.style.display = "none";
+    contentEdit.contentEditable = false;
 }
 const editComment = (userId) => {
+    document.getElementById(`button__update--${userId}`).onclick = () => updateComment(userId);
     console.log(userId);
     console.log(document.getElementById(`user__content--${userId}`));
     const contentEdit = document.getElementById(`user__content--${userId}`);
@@ -107,22 +109,55 @@ const editComment = (userId) => {
     selection.removeAllRanges();
     selection.addRange(range);
     buttonUpdate.style.display = "block";
-    buttonUpdate.addEventListener("click", () => updateComment(userId));
 }
-const addNewReply = (userId) => {
+const findNestedObj = (keyToFind, valToFind) =>{
+    let foundObj;
+    JSON.stringify(user_data, (_, nestedValue) => {
+      if (nestedValue && nestedValue[keyToFind] === valToFind) {
+        foundObj = nestedValue;
+      }
+      return nestedValue;
+    });
+    return foundObj;
+};
+const addNewReply = (userId, selectorName) => {
     event.preventDefault();
     console.log(userId);
     let content = document.getElementById(`reply__text--${userId}`);
     const contentValue = content.value.trim();
     console.log(contentValue);
     content.value = "";
+    const nextId = findNextId() + 1;
+    console.log("nextId", nextId);
+    const newComment = {
+        "id": nextId,
+        "content": contentValue,
+        "score": 0,
+        "user": user_data.currentUser,
+        "createdAt": "1 minute ago"
+    }
+    if(selectorName.id.substr(0, 7) === 'replies'){
+        userId = parseInt(selectorName.id.substr(7));
+        console.log("userId", userId);
+    }
+    user_data.comments.map((user) => {
+        if(user.id === userId){
+            user.replies.push(newComment);
+        }
+        return user;
+    });
+    console.log(user_data);
+    localStorage.setItem("data", JSON.stringify(user_data));
+    displayUser();
+
 }
-const replyComment = (userId) => {
+const replyComment = (userId, selectorName) => {
     document.getElementById(`reply__container--${userId}`).style.display = "grid";
-    document.getElementById(`reply--${userId}`).addEventListener("click", () => addNewReply(userId));
+    document.getElementById(`reply--${userId}`).addEventListener("click", () => addNewReply(userId, selectorName));
 }
 
-const userCode = (user, selectorName) => {
+const userCode = (user, selectorClassName, selectorIdName) => {
+    console.log(selectorIdName.slice(1));
     const userCode = `
     <div class="user__container">
         <div class="user" id="user--${user.id}">
@@ -143,18 +178,18 @@ const userCode = (user, selectorName) => {
                 ${user.content}
             </p>
             <div class="score" id="score--${user.id}">
-                <button class="score__button" id="button__increase--${user.id}" onclick="increaseScore(${user.id}, ${selectorName === ".replies"})">
+                <button class="score__button" id="button__increase--${user.id}" onclick="increaseScore(${user.id}, ${selectorClassName === ".replies"})">
                     <img src="./images/icon-plus.svg" alt="icon-plus" class="img__increase">
                 </button>
                     <span class="score__count" id="score__count--${user.id}">${user.score}</span>
-                <button class="score__button" id="button__decrease--${user.id}" onclick="decreaseScore(${user.id}, ${selectorName === ".replies"})">
+                <button class="score__button" id="button__decrease--${user.id}" onclick="decreaseScore(${user.id}, ${selectorClassName === ".replies"})">
                     <img src="./images/icon-minus.svg" alt="icon-minus" class="img__decrease">
                 </button>
             </div>
             <div class="container__button" id="container__button--${user.id}">
                 ${(user.user.username === user_data.currentUser.username) ?
                 `
-                        <button class="button button__delete" id="button__delete--${user.id}" onclick="deleteComment(${user.id}, ${selectorName === ".replies"})">
+                        <button class="button button__delete" id="button__delete--${user.id}" onclick="deleteComment(${user.id}, ${selectorClassName === ".replies"})">
                             <img src="./images/icon-delete.svg" alt="icon-delete" class="button__image button__image--delete">
                             <span class="button__value button__value--delete">Delete</span>
                         </button>
@@ -165,7 +200,7 @@ const userCode = (user, selectorName) => {
                     
                     ` :
                 `
-                        <button class="button button__reply" id="button__reply--${user.id}" onclick = "replyComment(${user.id})">
+                        <button class="button button__reply" id="button__reply--${user.id}" onclick = "replyComment(${user.id}, ${selectorIdName.slice(1)})">
                             <img src="./images/icon-reply.svg" alt="icon-reply" class="button__image button__image--reply">
                             <span class="button__value button__value--reply">Reply</span>
                         </button>
@@ -193,29 +228,31 @@ const userCode = (user, selectorName) => {
     </div>
     `
 
-    document.querySelector(selectorName).innerHTML += userCode;
+    document.querySelector(selectorIdName).innerHTML += userCode;
+
     // document.getElementById(`button__increase--${user.id}`).addEventListener("click", increaseScore);
 }
 const displayUser = () => {
     user_data = JSON.parse(localStorage.getItem("data"));
-    document.querySelector(".container").innerHTML = ``;
+    document.querySelector("#container").innerHTML = ``;
     for (user of user_data.comments) {
         // console.log(user.id);
-        userCode(user, ".container");
+        userCode(user, ".container", "#container");
         // console.log(user.id);
         // console.log(document.getElementById(`button__increase--${user.id}`).innerHTML);
         // document.querySelector(`#button__increase--${user.id}`).onclick = function(){
         //     alert("buttons is clicked");
         // };
-        if (user.replies.length !== 0) {
-            const repliesDiv = `
-                <div class="replies">
+        const repliesDiv = `
+                <div class="replies" id="replies${user.id}">
                 </div>
             `
-            document.querySelector(".container").innerHTML += repliesDiv;
+        if (user.replies !== undefined && user.replies.length !== 0) {
+            
+            document.querySelector("#container").innerHTML += repliesDiv;
             for (reply of user.replies) {
                 // console.log(reply.id);
-                userCode(reply, ".replies");
+                userCode(reply, ".replies" ,`#replies${user.id}`);
                 // console.log(reply.id);
                 // document.querySelector(`#button__increase--${reply.id}`).onclick = function(){
                 //     alert("buttons is clicked");
@@ -234,7 +271,7 @@ const displayUser = () => {
         </form>
     </div>
 `
-    document.querySelector(".container").innerHTML += currentUserCommentDiv;
+    document.querySelector("#container").innerHTML += currentUserCommentDiv;
     document.querySelector(".button--send").addEventListener("click", addNewComment);
 
 }
